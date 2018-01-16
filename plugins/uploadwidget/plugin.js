3cc49d0c1a13c5918fdeeaf8e89801cc1ce57bb1
Jacek Bogdanski <j.bogdanski@cksource.com>
2018-01-16 13:07:01 +0100

    Fixed loader abort workflow.

--- plugins/uploadwidget/plugin.js
+++ plugins/uploadwidget/plugin.js
@@ -249,13 +249,14 @@
 					id = this.wrapper.findOne( '[data-cke-upload-id]' ).data( 'cke-upload-id' ),
 					loader = uploads.loaders[ id ],
 					capitalize = CKEDITOR.tools.capitalize,
+					aborted = false,
 					oldStyle, newStyle;
 
 				loader.on( 'update', function( evt ) {
 					// Abort if widget was removed.
 					if ( !widget.wrapper || !widget.wrapper.getParent() ) {
 						if ( !editor.editable().find( '[data-cke-upload-id="' + id + '"]' ).count() ) {
-							loader.abort();
+							abort();
 						}
 						evt.removeListener();
 						return;
@@ -263,12 +264,32 @@
 
 					editor.fire( 'lockSnapshot' );
 
+					function abort() {
+						if ( aborted ) {
+							return;
+						}
+						if ( typeof widget.onAbort === 'function' ) {
+							widget.onAbort( loader );
+						}
+
+						aborted = true;
+						loader.abort();
+					}
+
 					// Call users method, eg. if the status is `uploaded` then
 					// `onUploaded` method will be called, if exists.
-					var methodName = 'on' + capitalize( loader.status );
+					var methodName = 'on' + capitalize( loader.status ),
+						unlock = false;
 
 					if ( typeof widget[ methodName ] === 'function' ) {
-						if ( widget[ methodName ]( loader ) === false ) {
+						if ( loader.status === 'abort' ) {
+							abort();
+							unlock = true;
+						} else if ( widget[ methodName ]( loader ) === false ) {
+							unlock = true;
+						}
+
+						if ( unlock ) {
 							editor.fire( 'unlockSnapshot' );
 							return;
 						}
